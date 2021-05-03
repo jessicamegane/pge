1690cc1089df01c7d39e4d1e6afcd7eab06cbcfe
Jessica Megane <jessicac@student.dei.uc.pt>
2021-05-03 23:00:18 +0100

    function to update the probabilities

---
+++ pge/core/update_probs/update.py
@@ -0,0 +1,41 @@
+
+def update_probs(best, lf, gram):
+    """ Function to update the probabilities of the PCFG,
+    based on the productions chosen to construct the selected individual.
+    """
+    gram_counter = best['gram_counter']
+    for key, val in gram_counter.items():
+        total = sum(val)
+        if total > 0:
+            l = [0] * len(val)
+            for pos in range(len(val)):
+                counter = val[pos]
+                old_prob = gram.get_dict[key][pos][1]
+                prob_updated = old_prob
+                if counter > 0:
+                    prob_updated = round(min(old_prob + lf * counter / total, 1.0),14)
+                elif counter == 0:
+                    prob_updated = round(max(old_prob - lf * prob_updated, 0.0),14)
+
+                l[pos] = prob_updated
+
+            # Adjust probabilities - divide the remaining equally by all productions
+            while(round(sum(l),3) != 1.0):
+                if sum(l) > 1.0:
+                    res = sum(l) - 1.0
+                    diff = res / len(l)
+                    for i in range(len(l)):
+                        new = round(l[i] - diff,14)
+                        l[i] = max(new,0.0)
+                elif sum(l) < 1.0 and sum(l) > 0.0:
+                    res = 1.0 - sum(l)
+                    diff = res / len(l)
+                    for i in range(len(l)):
+                        new = round(l[i] + diff,14)
+                        l[i] = min(diff,1.0)
+
+            for i in range(len(l)):
+                gram.get_dict[key][i][1] = l[i]
+
+    print("Probabilities updated")
+    print(gram.get_dict)
