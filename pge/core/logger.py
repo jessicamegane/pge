a84325e65d5335c0f7f64c2b68b57506b16178dc
Jessica Megane <jessicac@student.dei.uc.pt>
2021-05-03 23:00:57 +0100

    function to save progress to files

--- pge/core/logger.py
+++ pge/core/logger.py
@@ -1,10 +1,16 @@
-import os
+from core.parameters import params
 import json
+import os
 
-def save_state(run, gen, population, best, gram, params):
+def evolution_progress(run, gen, population, best, gram):
     """ Function that saves the grammar and population of the current generation and run. """
+    data = '%d,%s,%f,%f' %(gen,"".join(best['phenotype']), best['fitness'], best['tst_error'])
+    if params['VERBOSE']:
+        print(data)
+    save_progress_to_file(data)
+
     to_save = []
-    to_save.append({"grammar": gram.get_rules()})
+    to_save.append({"grammar": gram.get_dict()})
 
     if params['SAVE_POP']:
         for index, ind in enumerate(population):
@@ -16,15 +22,16 @@ def save_state(run, gen, population, best, gram, params):
     folder = params['PATH'] + 'last_' + str(run)
     if not os.path.exists(folder):
         os.makedirs(folder,  exist_ok=True)
-    open('%s/generation_%d.json' % (folder,(gen)), 'w').write(json.dumps(to_save))
+    with open('%s/generation_%d.json' % (folder,(gen)), 'w') as f:
+        f.write(json.dumps(to_save))
 
+def save_progress_to_file(data):
     # save info of best individual overall
-    f = open(params['PATH'] + "data.txt","a")
-    s = '\n%d,%s,%f,%f' %(gen,"".join(best['phenotype']), best['fitness'], best['tst_error'])
-    f.write(s)
-    f.close()
-
-def save_parameters(params):
+    with open(params['PATH'] + "data.txt", "a") as f:
+        f.write(data + '\n')
+        f.close()
+    
+def save_parameters():
     folder = params['PATH']
     params_lower = dict((k.lower(), v) for k, v in params.items())
     c = json.dumps(params_lower)
